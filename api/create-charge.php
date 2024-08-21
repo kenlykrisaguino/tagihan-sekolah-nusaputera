1a61cb3f913eb040d00c99b48e2838c57a164e01
Kenly Krisaguino <kenlykrisaguinosantoso10@gmail.com>
2024-08-21 14:56:02 +0700

    Updated API for Mandiri

--- api/create-charge.php
+++ api/create-charge.php
@@ -1,36 +1,28 @@
 <?php
 
 require_once '../config/midtrans/Midtrans.php';
+require_once '../config/parse-env.php';
 header('Content-Type: application/json');
 
-\Midtrans\Config::$serverKey = 'YOUR_SERVER_KEY';
+\Midtrans\Config::$serverKey = getenv('MIDTRANS_SERVER_KEY');
 \Midtrans\Config::$isProduction = false;
 \Midtrans\Config::$isSanitized = true;
 \Midtrans\Config::$is3ds = true;
 
-$params = [
-    'transaction_details' => [
-        'order_id' => '',
-        'gross_amount' => 10000,
-    ],
-    'item_details' => [
-        [
-            'id' => 'ITEM1',
-            'price' => 10000,
-            'quantity' => 1,
-            'name' => 'Midtrans Bear',
-        ],
-    ],
-    'customer_details' => [
-        'first_name' => 'TEST',
-        'last_name' => 'MIDTRANSER',
-        'email' => 'test@midtrans.com',
-        'phone' => '+628123456',
-    ],
-    'enabled_payments' => ['bri_va'],
-    'bri_va' => [
-        'va_number' => '47324297463927', 
-    ]
-];
+$params = array(
+    'payment_type' => 'bank_transfer',
+    'transaction_details' => array(
+        'order_id' => 'YOUR_ORDER_ID',
+        'gross_amount' => 10000, 
+    ),
+    'customer_details' => array(
+        'first_name' => 'John',
+        'phone' => '08123456789'
+    ),
+    'bank_transfer' => array(
+        'bank' => 'bni',
+        'account_number' => '1234567890'
+    ),
+);
 
-$chargeResponse = \Midtrans\Charge::create($params);
+$chargeResponse = \Midtrans\CoreApi::charge($params);
